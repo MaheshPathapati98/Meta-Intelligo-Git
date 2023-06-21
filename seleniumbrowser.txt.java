a9a33b4c7ac7978139ade261ba98f902da509b54
Maheshpathapati98 <Maheshpathapati279@gmail.com>
2023-06-21 07:37:45 +0530

    sample file

---
+++ seleniumbrowser.txt.java
@@ -0,0 +1,19 @@
+package test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class seleniumbrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//ChromeDriver driver = new ChromeDriver();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.tsrtconline.in");
+
+	}
+
+}
